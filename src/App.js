f0fd5c32f0b0e895f82624d5b7d85854b80a6ea2
TJ Gore <hello@tjwgore.com>
2021-03-08 10:14:02 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 text-base text-gray-600 font-normal trackind-wide font-sans min-h-screen">
+      <div className="flex justify-center min-h-screen items-center">
+        <h1 className="text-3xl font-semibold">React Tailwind Starter</h1>
+      </div>
     </div>
   );
 }
